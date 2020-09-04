cd1baca813d469beb45513bcc960e5112f12b8d8
William Penton <william@penton.us>
2020-09-04 09:12:23 -0500

    added more to welcome page

--- code-laravel/resources/views/welcome.blade.php
+++ code-laravel/resources/views/welcome.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="content">
-            <div class="title m-b-md">
-                Penton Posts!
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="title">What will you post today?</div>
+                    </div>
+                    <div class="card-body">
+                        Post messages and see what other post as well!
+                    </div>
+                </div>
             </div>
         </div>
     </div>
